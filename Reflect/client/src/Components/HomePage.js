6403ab34c5891aa39e0ab5ede02bd53bef15eef0
Christian Moore <christianmoore672@gmail.com>
2023-08-08 06:01:08 -0400

    homepage cute

--- Reflect/client/src/Components/HomePage.js
+++ Reflect/client/src/Components/HomePage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useEffect, useRef } from "react";
 import hair7 from "../assets/hair7.json"
+import face from "../assets/face.json"
 import computer from "../assets/computer.json"
+import swing from "../assets/swing.json"
 // import Lottie from "lottie-react"
 import lottie from "lottie-web";
 
@@ -15,21 +17,27 @@ export default function Reflect() {
       renderer: 'svg',
       loop: true,
       autoplay: true,
-      animationData: require('../assets/hair7.json'),
+      animationData: require('../assets/swing.json'),
       // animationData: require('../assets/computer.json')
     })
 }, [])
 
   return (
-    <article className="home"  >
+    <article className="home" ref={container} >
       
-      <article className="container"ref={container}>
+      <article className="container">
+      <div className="r"> R</div>
+      <div className="e"> E</div>
+      <div className="f"> F</div>
+      <div className="l"> L</div>
+      <div className="ee"> E</div>
+      <div className="c"> C</div>
+      <div className="t"> T</div>
       <div>
-      <div className="reflect"> REFLECT</div>
-          <button className="journal_Button">Journal</button>
-          <button className="trends_Button">Trends</button>
-          <button className="research_Button">Research</button>
-        </div>
+          <button className="journal_Button">JOURNAL</button>
+          <button className="trends_Button">MY TRENDS</button>
+          <button className="research_Button">RESEARCH</button>
+      </div>
     </article>
 
     </article>
